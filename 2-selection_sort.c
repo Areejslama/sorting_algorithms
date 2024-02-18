a1f8db4cfc69792bd0e9b77a4a4c42bec51f5eab
Areejslama <Areegahmed449@gmail.com>
2024-02-18 10:23:19 -0800

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,23 +13,20 @@ void selection_sort(int *array, size_t size)
 	size_t i, n, min;
 	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = i;
-
-        for (n = i + 1; n < size; n++)
-        {
-            if (array[n] < array[min])
-                min = n;
-        }
-
-        if (min != i)
-        {
-            temp = array[i];
-            array[i] = array[min];
-            array[min] = temp;
-
-            print_array(array, size);
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (n = i + 1; n < size; n++)
+		{
+			if (array[n] < array[min])
+				min = n;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 }
