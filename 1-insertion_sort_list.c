7f388cb5a0ec3e1e73acec6576a00718d81b8356
Areejslama <Areegahmed449@gmail.com>
2024-02-20 13:33:48 -0800

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,13 +11,12 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *t;
 	listint_t *h;
-
-	t = *list;
-	t = t->next;
-
+	
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
+	t = *list;
+	t = t->next;
+	
 	while (t != NULL)
 	{
 		while (t->prev != NULL && t->n < t->prev->n)
@@ -26,19 +25,19 @@ void insertion_sort_list(listint_t **list)
 			
 			if (t->next != NULL)
 				t->next->prev = h->prev;
-				t->prev->next = h->next;
-				t = t->prev;
-				h->prev = t->prev;
-				h->next = t;
-				
-				if (t->prev != NULL)
-					t->prev->next = h;
-				t->prev = h;
-				if (h->prev == NULL)
-					*list = h;
-				
-				print_list(*list);
-				t = t->prev;
+			t->prev->next = h->next;
+			t = t->prev;
+			h->prev = t->prev;
+			h->next = t;
+			
+			if (t->prev != NULL)
+				t->prev->next = h;
+			t->prev = h;
+			if (h->prev == NULL)
+				*list = h;
+			
+			print_list(*list);
+			t = t->prev;
 		}
 		t = t->next;
 	}
